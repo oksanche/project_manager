647ddfba60ad0c73f4565916bcb9aba13679dd81
Panfilova Oksana <oksssanche@gmail.com>
2017-06-27 21:36:51 +0300

    Изменены комментарии и структура БД.
    1. Переименованы таблицы tblusertask на tblusehasrtask и tbluserproject на  tblusershasprojects.
    2. Изменены типы полей и комментарии к полям.
    3.  Добавлены связи между таблицами.
    4. Добавлена информация о версии MySQL и кодировке сервера базы данных

--- dbstructure/projectmanager.sql
+++ dbstructure/projectmanager.sql
@@ -1,20 +1,28 @@
--- Время создания: Июн 27 2017 г., 15:19
--- Автор: Панфилова Оксана.
+-- Автор: Панфилова Оксана, oksssanche@gmail.com.
+-- Время создания: Июн 27 2017 г., 22:16
+-- Версия MySQL: 5.5.25
+-- Кодировка сервера: UTF-8 Unicode (utf8)
+
+SET time_zone = "+00:00";
+
+--
 -- База данных: `projectmanager`
 --
+CREATE DATABASE IF NOT EXISTS `projectmanager`;
+-- --------------------------------------------------------
 
 --
 -- Структура таблицы `tblproject`
---
--- Создание: Июн 27 2017 г., 00:51
 --
 
 CREATE TABLE IF NOT EXISTS `tblproject` (
   `bigintProjectId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор проекта',
   `varchProjectName` varchar(255) NOT NULL COMMENT 'Имя проекта',
-  `decProjectTime` decimal(18,2) NOT NULL COMMENT 'Общее время ',
-  `decProjectPercent` decimal(3,2) NOT NULL COMMENT 'Процент выполнения',
-  PRIMARY KEY (`bigintProjectId`)
+  `decProjectTime` decimal(18,2) NOT NULL DEFAULT '0.00' COMMENT 'Общее время в часах.',
+  `decProjectPercent` decimal(3,2) NOT NULL DEFAULT '0.00' COMMENT 'Процент выполнения',
+  `bigintUserId` bigint(20) NOT NULL COMMENT 'Создатель проекта',
+  PRIMARY KEY (`bigintProjectId`),
+  KEY `bigintUserId` (`bigintUserId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Проект'  ;
 
 -- --------------------------------------------------------
@@ -22,31 +30,21 @@ CREATE TABLE IF NOT EXISTS `tblproject` (
 --
 -- Структура таблицы `tbltask`
 --
--- Создание: Июн 27 2017 г., 01:08
---
 
 CREATE TABLE IF NOT EXISTS `tbltask` (
   `bigintTaskId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор',
   `varchTaskName` varchar(255) NOT NULL COMMENT 'Название',
-  `textTaskDescription` text NOT NULL COMMENT 'Описание задачи',
-  `intTaskStatus` int(1) DEFAULT NULL COMMENT '0-нерешенная,1- в процессе выполнения, 2- выполнена',
+  `textTaskDescription` text COMMENT 'Описание задачи',
+  `intTaskStatus` int(1) NOT NULL DEFAULT '0' COMMENT '0-нерешенная,1- в процессе выполнения, 2- выполнена',
   `bigintProjectId` bigint(20) NOT NULL COMMENT 'Какому проекту принадлежит задача',
   PRIMARY KEY (`bigintTaskId`),
   KEY `bigintProjectId` (`bigintProjectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8  ;
-
---
--- СВЯЗИ ТАБЛИЦЫ `tbltask`:
---   `bigintProjectId`
---       `tblproject` -> `bigintProjectId`
---
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Задача' ;
 
 -- --------------------------------------------------------
 
 --
 -- Структура таблицы `tbluser`
---
--- Создание: Июн 27 2017 г., 01:04
 --
 
 CREATE TABLE IF NOT EXISTS `tbluser` (
@@ -62,57 +60,43 @@ CREATE TABLE IF NOT EXISTS `tbluser` (
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `tbluserproject`
---
--- Создание: Июн 27 2017 г., 11:02
+-- Структура таблицы `tbluserhastask`
 --
 
-CREATE TABLE IF NOT EXISTS `tbluserproject` (
-  `bigintUserProjectId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор',
-  `bigintUserId` bigint(20) NOT NULL COMMENT 'Пользователь',
-  `bigintProjectId` bigint(20) NOT NULL COMMENT 'Проект, в котором участвует пользователь',
-  PRIMARY KEY (`bigintUserProjectId`),
-  KEY `bigintUserId` (`bigintUserId`,`bigintProjectId`),
-  KEY `bigintProjectId` (`bigintProjectId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 ;
-
---
--- СВЯЗИ ТАБЛИЦЫ `tbluserproject`:
---   `bigintUserId`
---       `tbluser` -> `bigintUserId`
---   `bigintProjectId`
---       `tblproject` -> `bigintProjectId`
---
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `tblusertask`
---
--- Создание: Июн 27 2017 г., 10:49
---
-
-CREATE TABLE IF NOT EXISTS `tblusertask` (
-  `bigintUserTaskId` bigint(20) NOT NULL COMMENT 'Идентификатор',
+CREATE TABLE IF NOT EXISTS `tbluserhastask` (
+  `bigintUserHasTaskId` bigint(20) NOT NULL COMMENT 'Идентификатор',
   `bigintUserId` bigint(20) NOT NULL COMMENT 'Идентификатор пользователя',
   `bigintTaskId` bigint(20) NOT NULL COMMENT 'Идентификатор задачи ',
-  `timestUserTaskBegin` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Начало выполнения задачи.',
-  `timestUserTaskEnd` datetime NOT NULL COMMENT 'Дата и время окончания выполнения задачи',
+  `datetimeUserHasTaskBegin` datetime NOT NULL COMMENT 'Начало выполнения задачи.',
+  `datetimeUserHasTaskEnd` datetime DEFAULT NULL COMMENT 'Дата и время окончания выполнения задачи',
   KEY `bigintUserId` (`bigintUserId`,`bigintTaskId`),
   KEY `bigintTaskId` (`bigintTaskId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Задачи, выполняемые пользователем';
 
+-- --------------------------------------------------------
+
 --
--- СВЯЗИ ТАБЛИЦЫ `tblusertask`:
---   `bigintUserId`
---       `tbluser` -> `bigintUserId`
---   `bigintTaskId`
---       `tbltask` -> `bigintTaskId`
+-- Структура таблицы `tblusersinprojects`
 --
+
+CREATE TABLE IF NOT EXISTS `tblusersinprojects` (
+  `bigintUsersInProjectId` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор',
+  `bigintUserId` bigint(20) NOT NULL COMMENT 'Пользователь',
+  `bigintProjectId` bigint(20) NOT NULL COMMENT 'Проект, в котором участвует пользователь',
+  PRIMARY KEY (`bigintUsersInProjectId`),
+  KEY `bigintUserId` (`bigintUserId`,`bigintProjectId`),
+  KEY `bigintProjectId` (`bigintProjectId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Участие пользователей в проектах' ;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
+
+--
+-- Ограничения внешнего ключа таблицы `tblproject`
+--
+ALTER TABLE `tblproject`
+  ADD CONSTRAINT `tblproject_ibfk_1` FOREIGN KEY (`bigintUserId`) REFERENCES `tbluser` (`bigintUserId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Ограничения внешнего ключа таблицы `tbltask`
@@ -121,15 +105,21 @@ ALTER TABLE `tbltask`
   ADD CONSTRAINT `tbltask_ibfk_1` FOREIGN KEY (`bigintProjectId`) REFERENCES `tblproject` (`bigintProjectId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ограничения внешнего ключа таблицы `tbluserproject`
+-- Ограничения внешнего ключа таблицы `tbluser`
 --
-ALTER TABLE `tbluserproject`
-  ADD CONSTRAINT `tbluserproject_ibfk_2` FOREIGN KEY (`bigintUserId`) REFERENCES `tbluser` (`bigintUserId`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tbluserproject_ibfk_1` FOREIGN KEY (`bigintProjectId`) REFERENCES `tblproject` (`bigintProjectId`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `tbluser`
+  ADD CONSTRAINT `tbluser_ibfk_1` FOREIGN KEY (`bigintUserId`) REFERENCES `tblproject` (`bigintUserId`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ограничения внешнего ключа таблицы `tblusertask`
+-- Ограничения внешнего ключа таблицы `tbluserhastask`
 --
-ALTER TABLE `tblusertask`
-  ADD CONSTRAINT `tblusertask_ibfk_2` FOREIGN KEY (`bigintUserId`) REFERENCES `tbluser` (`bigintUserId`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `tblusertask_ibfk_1` FOREIGN KEY (`bigintTaskId`) REFERENCES `tbltask` (`bigintTaskId`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `tbluserhastask`
+  ADD CONSTRAINT `tbluserhastask_ibfk_1` FOREIGN KEY (`bigintTaskId`) REFERENCES `tbltask` (`bigintTaskId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbluserhastask_ibfk_2` FOREIGN KEY (`bigintUserId`) REFERENCES `tbluser` (`bigintUserId`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `tblusersinprojects`
+--
+ALTER TABLE `tblusersinprojects`
+  ADD CONSTRAINT `tblusersinprojects_ibfk_1` FOREIGN KEY (`bigintProjectId`) REFERENCES `tblproject` (`bigintProjectId`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tblusersinprojects_ibfk_2` FOREIGN KEY (`bigintUserId`) REFERENCES `tbluser` (`bigintUserId`) ON DELETE CASCADE ON UPDATE CASCADE;
