cae2a91e3e7a9c01f0841e25e7e866cbec09f221
Panfilova Oksana <oksssanche@gmail.com>
2017-06-27 15:07:13 +0300

    Подключение модулей и параметры подключения к БД.
    Подключены express, mysql, body-parser. 
    Вывод порта и хоста в консоль при подключении.
    Добавлены параметры подключения к БД.
    Сделаны заготовки для get  и post запросов index.html

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-var fs = require('fs');
+var fs = require('fs'); 
+var express = require('express'); 
+var app = express(); 
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json() );   
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false }) 
+
+app.use(express.static('public'));
+app.use('/css', express.static('css'));
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	  host     : 'localhost',
+	  user     : 'root',
+	  password : 'panfilov',
+	  database : 'projectmanager'
+	});
+
+app.get('/index.html', function (req, res) {   
+   res.sendFile( __dirname + "/" +"index.html" ); 
+}) 
+
+
+app.post('/index.html',urlencodedParser, function (req, res, next) { 
+	
+}) 
+
+var server = app.listen(8080, function () { 
+var host = server.address().address 
+var port = server.address().port 
+
+console.log("ProjectManager app listening at http://%s:%s", host, port) 
+})
