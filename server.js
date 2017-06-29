ab1bfce73803b6626d794c044a9de7969a99d185
Oksana Panfilova <oksssanche@gmail.com>
2017-06-29 15:46:01 +0300

    Сделали куки и загрузили новые node-модули

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ var app = express();
 
 app.use(express.static('public'));
 app.use('/css', express.static('css'));
-
 app.use('/view', express.static('css'));
 
 var bodyParser = require('body-parser');
+app.use( bodyParser.json() );
 
-app.use( bodyParser.json() );  
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false }) 
+//куки парсер
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 var mysql = require('mysql');
 
@@ -20,15 +22,48 @@ var connection = mysql.createConnection({
 				  password : 'panfilov',
 				  database : 'projectmanager'
 				});
+
  connection.connect(function(err) {
 	  if (err) throw err;
 	  console.log("Connected!");
  }) 
  
+ // app.use(express.session({
+	 // secret:'1';
+	 // store:
+ 
 app.get('/login', function (req, res) {   
-   res.sendFile( __dirname + "/" +"index.html" ); 
-}) 
 
+if (req.cookies.user === undefined) {
+        res.cookie('user', '0');
+        console.log('setting cookie to false');
+		res.sendFile( __dirname + "/" +"index.html" );
+    }
+
+    if (req.cookies.user === '1') {
+        res.redirect('/mainpage');
+        return;
+    } 
+	else {
+		 res.sendFile( __dirname + "/" +"index.html" );
+	}
+}) 
+app.get('/mainpage', function (req, res) {  
+
+ if (req.cookies.user === undefined) {
+        res.cookie('user', '0');
+        console.log('setting cookie to false');		
+    }
+	
+    if (req.cookies.user === '1') {
+        res.sendFile( __dirname + "/view/" +"mainpage.html"); 
+        return;
+    } 
+	else {
+		res.redirect('/login');
+	}
+ 
+}) 
 //Запрос			
 app.get('/registration', function (req, res) {   
    res.sendFile( __dirname + "/view/" +"registration.html" ); 
@@ -41,6 +76,11 @@ app.get('/registration', function (req, res) {
  app.get('/new_project.html', function (req, res) {   
    res.sendFile( __dirname + "/view/" +"new_project.html" ); 
 }) 
+
+ app.post('/new_project.html', function (req, res) {   
+   res.sendFile( __dirname + "/view/" +"new_project.html" ); 
+}) 
+
  app.get('/tasks.html', function (req, res) {   
    res.sendFile( __dirname + "/view/" +"tasks.html" ); 
 }) 
@@ -69,7 +109,7 @@ var error={
 		//Ошибка: если результат select-запроса не пуст: пользователь уже существует		
 			 
 			 if (rows.length) {		
-                 console.log("ЗАНЯТО");
+                 console.log("Такой email уже занят. Попробуйте снова");
 				 error.message="Такой email уже занят. Попробуйте снова";
 				 res.send(JSON.stringify(error,"", 3));
             } else {
@@ -87,58 +127,65 @@ var error={
 			}
 });
 })
-app.post('/login', urlencodedParser,function (req, res) {   
+app.post('/mainpage', urlencodedParser, function (req, res) {   
+				 
+				 
 			var email=req.body.email;
 			var password=req.body.password;
-			
-			var error={
-				code:0,
-				message:""
-			};
-			
+		
          connection.query("SELECT * FROM `tbluser` WHERE `varchUserEmail` = '" + email + "'", function(err,rows){
+			 
 			if (err){
-				error.code=-1;
-				error.message=err;
+				res.redirect("/login");	
 				console.log(err);
 			}
                
 			 if (!rows.length) { 
-			    error.code=-1;
-				error.message="Нет такого пользователя";
-				res.status(500);
-			
-             console.log('нет такого пользователя');
+				res.redirect("/login");	
+                console.log('Нет такого пользователя');
 			 //message
 			} 
 				
              // если пользователь найден, но пароль неверен
             if (!( rows[0].varchUserPassword == password)){
-				error.code=-1;
-				
-			  	res.redirect("/login");
-				
-				//error.message="Неверный пароль";	
-
-			//	res.send(JSON.stringify(error,"", 3));
-             //  console.log("НЕВЕРНЫЙ ПАРОЛЬ");
+				 console.log("Неверный пароль");
+		         res.redirect("/login");	
 			}
 			else {
-            res.sendFile( __dirname + "/view/" +"mainpage.html" ); 
-			}
-            // Все хорошо, возвращаем успешного пользователя
-           // return done(null, rows[0]);			
-		
+				// req.session.email = request.body.email;
+				// res.cookie('id', rows[0].varchUserId);
+				 res.cookie('user', '1');
+				 res.redirect("/mainpage");
+				 }
 		});
 	})
 
 app.get('/', function (req, res) {
-    res.redirect('/login');
+	
+	console.log('Мы в /');
+    console.log(req.cookies.user);
+    console.log(req.cookies.user === '0');
+    console.log('конец');
+	
+	//если куков не было ваще никаких
+    if (req.cookies.user === undefined) {
+        res.cookie('user', '0');
+        console.log('setting cookie to false');
+    }
+
+    if (req.cookies.user === '1') {
+         res.redirect('/mainpage');
+    } else {
+        res.redirect('/login');
+    }
 });
 
-app.get('/index.html', function (req, res) {
-    res.redirect('/login');
+app.get('/logout', function (req, res) {
+	 res.cookie('user', undefined);
+	 console.log(req.cookies.user);
+     res.redirect('/login');
 });
+
 app.get('*', function(req, res){
    res.status(404).send('Cтраница не найдена! Возможно, запрашиваемая Вами страница была перенесена или удалена. Или Вы допустили небольшую опечатку при вводе адреса, поэтому еще раз внимательно проверьте.');
 });
