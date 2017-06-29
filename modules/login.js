dddc220464ea5c396f84d36abd8e892878f7da8b
Oksana Panfilova <oksssanche@gmail.com>
2017-06-29 23:47:24 +0300

    обавила аргумент

---
+++ modules/login.js
@@ -0,0 +1,32 @@
+//Автор: Панфилова Оксана
+
+ module.exports= function (db, req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    db.query("SELECT * FROM `tbluser` WHERE `varchUserEmail` = '" + email + "'", function(err, rows) {
+
+        if (err) {
+            res.redirect("/login");
+            console.log(err);
+        }
+
+        if (!rows.length) {
+            res.redirect("/login");
+            console.log('Нет такого пользователя');
+            //message
+        }
+
+        // если пользователь найден, но пароль неверен
+        if (!(rows[0].varchUserPassword == password)) {
+            console.log("Неверный пароль");
+            res.cookie('user', '0');
+            res.redirect("/login");
+        } else {
+            res.cookie('userid', rows[0].bigintUserId);
+            res.cookie('username', rows[0].varchUserFirstName + " " + rows[0].varchUserFirstName);
+            res.cookie('user', '1');
+            res.redirect("/mainpage");
+        }
+    });
+};
