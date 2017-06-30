f7ce7286e6455b3fe7d97c5030cedfbdc6304004
Oksana Panfilova <oksssanche@gmail.com>
2017-06-30 04:30:18 +0300

    Модуль для получения списка проектов, которыми управляет пользователь.

---
+++ modules/listofprojects.js
@@ -0,0 +1,16 @@
+﻿// Автор: Оксана Панфилова
+module.exports = function (db, req, res){
+    var id = parseInt(req.cookies.userid);
+	
+    var insertQuery = "SELECT varchProjectName from tblproject WHERE bigintUserId=" + id + "";
+console.log(insertQuery);
+
+    db.query(insertQuery, function(err, rows) {
+        if (err) {
+            console.log(err);
+			//res.send("");
+        }
+		var records = {rows};
+		res.send(JSON.stringify(records));
+	});
+};
