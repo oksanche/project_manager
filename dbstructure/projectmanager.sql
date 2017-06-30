630bb013f4b5db3f332c35a91ea50a1a8d8b9ccb
Oksana Panfilova <oksssanche@gmail.com>
2017-06-30 12:30:28 +0300

    USE projectmanager;
    Добавила команду USE projectmanager, чтобы все разом можно было запустить.

--- dbstructure/projectmanager.sql
+++ dbstructure/projectmanager.sql
@@ -11,6 +11,7 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `projectmanager`;
 -- --------------------------------------------------------
 
+USE projectmanager;
 --
 -- Структура таблицы `tblproject`
 --
