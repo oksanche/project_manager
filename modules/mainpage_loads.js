9c3597ebdad6c941d15e7f5457fd3d77c5f1b446
Илья Ерофеев <nestle3195@yandex.ru>
2017-06-30 11:47:58 +0300

    стиль гл страницы н съезжает + файл модуля

---
+++ modules/mainpage_loads.js
@@ -0,0 +1,14 @@
+// Автор: Ерофеев Илья
+
+function load_administration() {
+  $('#content').load('administration.html');
+}
+function load_statistics() {
+  $('#content').load('statistics.html'); 
+}
+function load_tasks() {
+  $('#content').load('tasks.html');
+}
+function load_new_project() {
+  $('#content').load('new_project.html');
+}
